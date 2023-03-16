308953b0f3f6948534a6581e22d4320f489c37b5
eric <eroija@gmail.com>
2023-03-16 10:16:55 -0700

    hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 /* A C program that prints text */
 #include <stdio.h>
+/**
+ * main - main entry point
+ * return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0); 
+}		
 
-main()
-{	
-    /* String initialization */
-    char str[] = "Programming is like building a multilingual puzzle;
-    puts(str); /* Wrrting the string to stdout */
-    return 0
-}	    
