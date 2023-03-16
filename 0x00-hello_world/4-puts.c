fe2e5f1bfbd1dc2772205c5359f9d409baec66b4
eric <eroija@gmail.com>
2023-03-16 06:07:27 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
 #!/bin/bash
-chr str[] = "Programming is like building a multilingual puzzle"; puts(str);
+#include <stdio.h>
+
+int main()
+{       char str[] = "Programming is like building a multilingual puzzle\n";
+	puts(str)
+        return 0
+}	
