fb98703f86ad484b49f3c20435e088033905c60a
Eric <eroija@gmail.com>
2023-03-17 22:25:43 +0300

    correct

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,14 @@
  *
  * Return: always 0
  */
- 
 int main(void)
 {
-        char i;
-	for ( i = 'a'; i <= 'z'; i++)
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
 		putchar(i)
-	for (i = 'A'; i <= 'Z'; i++)	
+	for (i = 'A'; i <= 'Z'; i++)
 		putchar(i)
 	putchar('\n');
 	return (0);
-}	
-
-	    		
+}
