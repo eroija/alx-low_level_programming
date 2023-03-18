ad4932208168994fe48a2634f423c762f68b0a9e
Eric <eroija@gmail.com>
2023-03-18 04:07:31 +0300

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - Entry point
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int b;
+	char low;
+
+	for (b = '0'; b <= '9'; b++)
+		putchar(b);
+	putchar('\n');
+
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}
