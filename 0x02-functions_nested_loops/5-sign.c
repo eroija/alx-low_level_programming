1908dbec5dcd32a45adf2501455ee9f0b0e9b2e8
Eric <eroija@gmail.com>
2023-03-21 16:03:19 +0300

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,21 +12,24 @@
 int print_sign(int n)
 {
 	if (n > o)
+	{
 		_putchar('+');
 		_putchar(',');
 		_putchar(' ');
 		return (1);
-
+	}
 	else if (n == 0)
+	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
 		return (0);
-
-
+	}
 	else
+	{
 		_putchar('-');
 		_putchar(',');
 		_putchar(' ');
 		return (-1);
+	}
 }
