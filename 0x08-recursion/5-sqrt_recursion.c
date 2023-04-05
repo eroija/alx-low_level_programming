aa1ba62f2f5e5956ab4deee0f9bd0a1d2cfc9cb2
Eric <eroija@gmail.com>
2023-04-06 02:57:04 +0300

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,4 +2,30 @@
 /**
  * _sqrt_recursion - function that returns the natural
  * square root of a number
- * 
+ * @n: base number
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqr(n, 1));
+}
+
+
+/**
+ * sqr - function that returns natural square root
+ * @n: number
+ * @i: iterator
+ * Return: square root
+ */
+
+int sqr(int n, int i)
+{
+	if (n * n == i)
+		return (n);
+	else if (n == i)
+		return (-1);
+	else
+		return (sqr(n, i + 1));
+
+}
