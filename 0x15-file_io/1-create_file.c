ca4fdeb141f0ab10b61ea73ce7bf52a401fde325
Eric <eroija@gmail.com>
2023-09-04 14:54:22 +0300

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: file to be created
+ * @text_content: a null terminating string
+ * to write to the file
+ * Return: 1 0n success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int f, wr, i;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i];)
+			i++;
+	}
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(f, text_content, i);
+
+	if (f == -1 || wr == -1)
+		return (-1);
+
+	close(f);
+	return (1);
+}
