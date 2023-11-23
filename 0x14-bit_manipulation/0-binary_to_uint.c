9c696d01a29d707a169e9c6c512717c2568b4ae1
Eric <eroija@gmail.com>
2023-11-23 12:21:20 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,11 +15,10 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i]; i++) /* loop through the string b to check characters */
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		j = 2 * j + (b[i] - '0');
 	}
 	return (j);
-}
