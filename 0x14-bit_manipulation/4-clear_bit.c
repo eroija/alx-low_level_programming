07ff7a30b0271f16231b352e2c4c20c7051cbb21
Eric <eroija@gmail.com>
2023-11-23 15:39:28 +0300

    4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,10 +14,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > 64)
 		return (-1);
 
-	if ((*n >> index) & 1)
-	{	
-		*n -= 1 << index;
-	}	
-
+	*n &= ~(1 << index);
 	return (1);
 }
